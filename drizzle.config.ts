99c07668166128d28037e0503c28d8e2ac941c51
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2025-03-19 10:58:47 -0400

    remove use of dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,14 @@
-import { config } from 'dotenv'
 import { defineConfig } from 'drizzle-kit'
 
-config({ path: '.env' })
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in your .env file')
+}
 
 export default defineConfig({
   schema: './db/schema.ts',
   out: './migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 })
