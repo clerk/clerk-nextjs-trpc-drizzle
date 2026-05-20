c5f7193eaf67535db83cb2c34f7ba4b319fd62c1
Alexis Aguilar <98043211+alexisintech@users.noreply.github.com>
2026-05-19 18:28:31 -0700

    styling updates

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -10,7 +10,7 @@ export default function Posts() {
 
   return (
     <div className="relative flex max-w-2xl flex-col rounded-lg border border-neutral-200 dark:border-neutral-800 shadow-md">
-      <div className="h-[calc(3*theme(spacing.24))] overflow-y-auto px-4 pb-6">
+      <div className="h-[calc(3*theme(spacing.24))] overflow-y-auto px-4 pt-2 pb-6">
         {isLoading && <div className="p-4">Loading...</div>}
         {!isLoading && posts?.length === 0 && <div className="p-4">No posts found</div>}
         {!isLoading &&
@@ -18,7 +18,7 @@ export default function Posts() {
             <Link
               key={post.id}
               href={`/posts/${post.id}`}
-              className="hover:bg-neutral-100 dark:hover:bg-neutral-800 flex flex-col rounded-lg px-2 py-4 transition-all hover:underline"
+              className="hover:bg-neutral-100 dark:hover:bg-neutral-800 flex flex-col rounded-lg px-2 pt-3 pb-4 transition-all hover:underline"
             >
               <span className="text-lg font-semibold">{post.title}</span>
               <span className="text-sm">by {post.authorId}</span>
